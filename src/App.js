7c5ffd1291dc2f51b9b385fe2939136b0e775f42
AlecZeng <zxh93321@gmail.com>
2019-04-02 14:28:31 +1300

    to do list

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-gitimport React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react';
 import './App.css';
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/es/FormControl";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(prop) {
+        super(prop);
+        this.state = {
+            toDoItem: '',
+            items: ['Study English', 'Study React']
+        };
+    }
+
+    render() {
+        return (
+
+
+            <Fragment>
+                <Container>
+                    <h1>To do list</h1>
+
+                    <InputGroup className="mb-3">
+                        <FormControl value={this.state.toDoItem} onChange={this.handleInputValue.bind(this)}/>
+                        <button className="btn btn-primary" onClick={this.addItem.bind(this, this.state.toDoItem)}>
+                            Add item
+                        </button>
+                    </InputGroup>
+                    <ListGroup>
+                        {this.state.items.map((item, index) =>
+                            <ListGroup.Item key={index}>{item}
+                                <button className="btn btn-danger float-right"
+                                        onClick={this.onDeleteItem.bind(this, index)}>Delete
+                                </button>
+                            </ListGroup.Item>
+                        )}
+                    </ListGroup>
+                </Container>
+            </Fragment>
+        );
+    }
+
+    addItem() {
+        this.setState({toDoItem: '', items: [...this.state.items, this.state.toDoItem]});
+    }
+
+    handleInputValue(event) {
+        this.setState({toDoItem: event.target.value})
+    }
+
+    onDeleteItem(index) {
+        const items = [...this.state.items];
+        items.splice(index, 1);
+        this.setState({items: items})
+    }
 }
 
 export default App;
