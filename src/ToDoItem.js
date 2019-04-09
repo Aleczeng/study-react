329a76fb8cea80afaffa44be2e4e24bc912959cf
AlecZeng <zxh93321@gmail.com>
2019-04-09 21:10:25 +1200

    todo item

---
+++ src/ToDoItem.js
@@ -0,0 +1,66 @@
+import React, {Component, Fragment} from 'react';
+import './App.css';
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/es/FormControl";
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: ['Study English', 'Study React']
+        };
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <Container>
+                    <h1>To do list</h1>
+                    <InputGroup className="mb-3">
+                        <label htmlFor="item">Enter item</label>
+                        <FormControl id="item" as="input" ref={(input) => {
+                            this.item = input
+                        }}/>
+                        <button className="btn btn-primary"
+                                onClick={() => {
+                                    this.addItem(this.item)
+                                }}>
+                            Add item
+                        </button>
+                    </InputGroup>
+                    <ListGroup>
+                        {this.state.items.map((item, index) =>
+                            <ListGroup.Item key={index}>{item}
+                                <button className="btn btn-danger float-right"
+                                        onClick={() => {
+                                            this.onDeleteItem()
+                                        }}>Delete
+                                </button>
+                            </ListGroup.Item>
+                        )}
+                    </ListGroup>
+                </Container>
+            </Fragment>
+        );
+    }
+
+    addItem(myInputValue) {
+        const items = [...this.state.items];
+        items.push(myInputValue.value);
+        this.setState({items});
+        myInputValue.value = '';
+    }
+
+
+    onDeleteItem(index) {
+        const items = [...this.state.items];
+        items.splice(index, 1);
+        this.setState({items})
+    }
+}
+
+export default App;
